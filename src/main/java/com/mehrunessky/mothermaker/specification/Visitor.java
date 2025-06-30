2ccf2012756afa02bb69f483768073fb12970941
elie <delpierreelie@gmail.com>
2025-06-30 23:04:10 +0200

    wip

--- src/main/java/com/mehrunessky/mothermaker/specification/Visitor.java
+++
@@ -1,7 +0,0 @@
-package com.mehrunessky.mothermaker.specification;
-
-public interface Visitor<T, R> {
-    R visit(DataGenerationSpecification.FromPredicate<T> tPredicate);
-
-    R visit(DataGenerationSpecification.And<T> tAnd);
-}
